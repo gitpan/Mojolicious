5343ac133a1522359ca2dade1e0bab75d3007f55
Sebastian Riedel <kraihx@googlemail.com>
2011-08-12 14:54:00 +0000

    Import of SRI/Mojolicious-1.76 from CPAN.
    gitpan-cpan-distribution: Mojolicious
    gitpan-cpan-version:      1.76
    gitpan-cpan-path:         SRI/Mojolicious-1.76.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- lib/Mojo/Path.pm
+++ lib/Mojo/Path.pm
@@ -8,27 +8,12 @@ use overload
 use Mojo::Util qw/url_escape url_unescape/;
 use Mojo::URL;
 
-has [qw/leading_slash trailing_slash/] => 0;
+has [qw/leading_slash trailing_slash/];
 has parts => sub { [] };
 
 sub new {
   my $self = shift->SUPER::new();
   $self->parse(@_);
-  return $self;
-}
-
-sub append {
-  my $self = shift;
-
-  for (@_) {
-    my $value = "$_";
-
-    # *( pchar / "/" / "?" )
-    url_escape $value, $Mojo::URL::PCHAR;
-
-    push @{$self->parts}, $value;
-  }
-
   return $self;
 }
 
@@ -79,8 +64,8 @@ sub parse {
 
   # Leading and trailing slash
   $path = '' unless defined $path;
-  $path =~ /^\// ? $self->leading_slash(1)  : $self->leading_slash(0);
-  $path =~ /\/$/ ? $self->trailing_slash(1) : $self->trailing_slash(0);
+  $path =~ /^\// ? $self->leading_slash(1)  : $self->leading_slash(undef);
+  $path =~ /\/$/ ? $self->trailing_slash(1) : $self->trailing_slash(undef);
 
   # Parse
   url_unescape $path;
@@ -112,10 +97,8 @@ sub to_string {
   # Escape
   my @path;
   for my $part (@{$self->parts}) {
-
-    # *( pchar / "/" / "?" )
     my $escaped = $part;
-    url_escape $escaped, $Mojo::URL::PCHAR;
+    url_escape $escaped, "$Mojo::URL::UNRESERVED$Mojo::URL::SUBDELIM\:\@";
     push @path, $escaped;
   }
 
@@ -182,12 +165,6 @@ following new ones.
   my $path = Mojo::Path->new('/foo/bar%3B/baz.html');
 
 Construct a new L<Mojo::Path> object.
-
-=head2 C<append>
-
-  $path = $path->append(qw/foo bar/);
-
-Append parts to path.
 
 =head2 C<canonicalize>
 
