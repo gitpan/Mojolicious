c8b3fa15fd7ca2e4000a3a33233fee10d49f5955
Sebastian Riedel <kraihx@googlemail.com>
2014-11-22 03:52:56 +0000

    Import of SRI/Mojolicious-5.63.tar.gz from CPAN.
    gitpan-cpan-distribution: Mojolicious
    gitpan-cpan-version:      5.63
    gitpan-cpan-path:         SRI/Mojolicious-5.63.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- lib/Mojo/Message.pm
+++ lib/Mojo/Message.pm
@@ -99,7 +99,8 @@ sub fix_headers {
   # Content-Length or Connection (unless chunked transfer encoding is used)
   my $content = $self->content;
   my $headers = $content->headers;
-  return $self if $content->is_chunked || $headers->content_length;
+  if ($content->is_multipart) { $headers->remove('Content-Length') }
+  elsif ($content->is_chunked || $headers->content_length) { return $self }
   if   ($content->is_dynamic) { $headers->connection('close') }
   else                        { $headers->content_length($self->body_size) }
 
